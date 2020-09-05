8b43c10591042c9735efd8e593278c633c2b9287
Mr.Seven590 <mr_seven590@qq.com>
2020-09-05 19:10:12 +0800

    Update diy-part1.sh
    back

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,17 +15,3 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-# Modify default IP
-sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
-sed -i 's/#src-git helloworld https://github.com/fw876/helloworld/src-git helloworld https://github.com/fw876/helloworld/g' /feeds.conf.default
-
-#移除不用软件包
-#rm -rf xxx/xxx/xxx/文件名
-
-#添加额外软件包
-git clone https://github.com/cnzd/luci-app-koolproxyR.git package        #去广告最新KPR
-git clone https://github.com/xiaorouji/openwrt-package.git xiaorouji     #常用软件集合包
-git clone https://github.com/kenzok8/openwrt-packages.git                #常用软件集合包二
-git clone https://github.com/destan19/OpenAppFilter.git                  #APP过滤应用
-git clone https://github.com/jerrykuku/luci-theme-argon.git       #取消注释下载最新的argon主题
