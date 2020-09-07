129fb813d409293ca0fbcceedf2b2e540b650f84
Mr.Seven590 <mr_seven590@qq.com>
2020-09-07 11:02:49 +0800

    Update diy-part2.sh
    增加最新argon主题

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,10 +17,11 @@ sed -i 's/192.168.2.1/192.168.2.100/g' package/base-files/files/bin/config_gener
 
 #移除不用软件包
 #rm -rf xxx/xxx/xxx/文件名
+rm -rf lede/package/lean/luci-theme-argon
 
 #添加额外软件包
-git clone https://github.com/cnzd/luci-app-koolproxyR.git package        #去广告最新KPR
-git clone https://github.com/xiaorouji/openwrt-package.git xiaorouji     #常用软件集合包
-git clone https://github.com/kenzok8/openwrt-packages.git                #常用软件集合包二
-git clone https://github.com/destan19/OpenAppFilter.git                  #APP过滤应用
-git clone https://github.com/jerrykuku/luci-theme-argon.git       #取消注释下载最新的argon主题
+#git clone https://github.com/cnzd/luci-app-koolproxyR.git package        #去广告最新KPR
+#git clone https://github.com/xiaorouji/openwrt-package.git xiaorouji     #常用软件集合包
+#git clone https://github.com/kenzok8/openwrt-packages.git                #常用软件集合包二
+#git clone https://github.com/destan19/OpenAppFilter.git                  #APP过滤应用
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean       #取消注释下载最新的argon主题
